644400940bc323b00da20dae614e48a8dabbb0d5
joriyann <31510902+joriyann@users.noreply.github.com>
2020-06-01 12:49:43 +0200

    Update get-credit-adobe-stock.js

--- get-credit-adobe-stock.js
+++ get-credit-adobe-stock.js
@@ -3,20 +3,16 @@
 // https://caiorss.github.io/bookmarklet-maker/
 (async () => {
 
-  var win = window.open('')
-  if (!win) {
-    window.alert('Veuillez désactiver le bloqueur de popup.')
-    return
-  }
+  var result = ''
 
   var title = `Crédit photo ${document.querySelector('#titleRenameContainer > p:nth-child(1)').innerText}`
-  win.document.write(`<title>${title}</title><style>body{font-family:"TrebuchetMS",Arial,Helvetica,sans-serif;}table{border-collapse:collapse;width:100%;}td{border:1px solid #ddd;padding:8px;}tr:nth-child(even){background-color:#f2f2f2;}tr:hover{background-color:#ddd;}</style><h1>${title}</h1>`)
+  result += `<title>${title}</title><style>body{font-family:"TrebuchetMS",Arial,Helvetica,sans-serif;}table{border-collapse:collapse;width:100%;}td{border:1px solid #ddd;padding:8px;}tr:nth-child(even){background-color:#f2f2f2;}tr:hover{background-color:#ddd;}</style><h1>${title}</h1>`
 
   var items = Array
     .from(document.querySelectorAll('div.search-result-cell:not([style*="none"])'))
-    .map(e => ({ id: e.dataset.contentId, img: e.querySelector('a > img').src }))
+    .map(e => ({ id: e.dataset.contentId, img: e.querySelector('a > img').dataset.lazy }))
 
-  win.document.write(`<p>${items.length} images</p>`)
+  result += `<p>${items.length} images</p>`
 
   var response = await fetch('/fr/Ajax/Library/getContents', {
     body: items.map(i => `content_ids[]=${i.id}`).join('&'),
@@ -29,12 +25,24 @@
 
   var data = await response.json()
 
-  win.document.write('<table>')
+  result += '<table>'
 
   items.forEach((item, index) => {
     var file = data.files[item.id]
-    win.document.write(`<tr><td>${index + 1}</td><td width="175"><img width="150"src="${item.img}"></td><td width="300">${file.author}/Adobestock</td><td>${file.title}</td></tr>`)
+    result += `<tr><td>${index + 1}</td><td width="175"><img width="150"src="${item.img}"></td><td width="300">${file.author}/Adobestock</td><td>${file.title}</td></tr>`
   })
 
-  win.document.write('</table>')
+  result += '</table>'
+
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/html;charset=utf-8,' + encodeURIComponent(result));
+  element.setAttribute('download', `${title}.htm`);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+
 })()
